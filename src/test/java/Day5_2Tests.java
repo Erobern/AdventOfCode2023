8dd14ff8fb07e2adb0bf024019ec7c9e35c7ac54
Garrett Rieger <garrettrieger@gmail.com>
2023-12-05 13:20:38 -0600

    unit test cleanup

--- src/test/java/Day5_2Tests.java
+++ src/test/java/Day5_2Tests.java
@@ -1,4 +1,5 @@
 import adventofcode2023.solvers.Day5;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -13,7 +14,7 @@ public class Day5_2Tests {
                 List.of(new Day5.TranslationMap(new BigInteger("15"), new BigInteger("0"), new BigInteger("15")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("5"), new BigInteger("5"))));
     }
 
     @Test
@@ -24,7 +25,7 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("15"), new BigInteger("0"), new BigInteger("2")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("5"), new BigInteger("5"))));
     }
 
     @Test
@@ -35,7 +36,7 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("30"), new BigInteger("0"), new BigInteger("15")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("1"), new BigInteger("10"))));
     }
 
     @Test
@@ -46,7 +47,9 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("15"), new BigInteger("0"), new BigInteger("2")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("3"), new BigInteger("4")),
+                new Day5.InputMap(new BigInteger("0"), new BigInteger("2")),
+                new Day5.InputMap(new BigInteger("7"), new BigInteger("6"))));
     }
 
     @Test
@@ -57,7 +60,8 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("10"), new BigInteger("3")),
+                new Day5.InputMap(new BigInteger("0"), new BigInteger("7"))));
     }
 
     @Test
@@ -68,7 +72,8 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("10"), new BigInteger("3")),
+                new Day5.InputMap(new BigInteger("0"), new BigInteger("7"))));
     }
 
     @Test
@@ -79,7 +84,8 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("7"), new BigInteger("3")),
+                new Day5.InputMap(new BigInteger("13"), new BigInteger("7"))));
     }
 
     @Test
@@ -90,7 +96,7 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("0"), new BigInteger("10"))));
     }
 
     @Test
@@ -101,7 +107,7 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("0"), new BigInteger("11"))));
     }
 
     @Test
@@ -112,7 +118,8 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("6"), new BigInteger("5")),
+                new Day5.InputMap(new BigInteger("0"), new BigInteger("16"))));
     }
 
     @Test
@@ -123,6 +130,16 @@ public class Day5_2Tests {
                         new Day5.TranslationMap(new BigInteger("50"), new BigInteger("0"), new BigInteger("5")))
         );
 
-        System.out.println(results);
+        assertInputMap(results, List.of(new Day5.InputMap(new BigInteger("0"), new BigInteger("16"))));
+    }
+
+    private void assertInputMap(List<Day5.InputMap> results, List<Day5.InputMap> inputMaps) {
+        Assertions.assertEquals(inputMaps.size(), results.size());
+
+        for (int i = 0; i < inputMaps.size(); i++) {
+            Assertions.assertEquals(inputMaps.get(i).input().compareTo(results.get(i).input()), 0);
+            Assertions.assertEquals(inputMaps.get(i).range().compareTo(results.get(i).range()), 0);
+
+        }
     }
 }
