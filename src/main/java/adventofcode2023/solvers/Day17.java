d5b0d0a023097f688eb912b87b71270e9994b721
Garrett Rieger <garrett.rieger@fcc-fac.ca>
2025-10-16 15:29:10 -0600

    day 17 p2

--- src/main/java/adventofcode2023/solvers/Day17.java
+++ src/main/java/adventofcode2023/solvers/Day17.java
@@ -125,7 +125,102 @@ public class Day17 {
     }
 
     public static String Puzzle2(String input) {
-     return null;
+
+        List<String> lines = FileLoaders.loadInputIntoStringList(input);
+
+        Integer[][] grid = new Integer[lines.size()][lines.getFirst().length()];
+
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.getFirst().length(); j++) {
+                grid[i][j] = Integer.parseInt(String.valueOf(lines.get(i).charAt(j)));
+            }
+        }
+
+        Coordinate finalCoordinate = new Coordinate(lines.size() - 1, lines.getFirst().length() - 1);
+
+        HashMap<String, Integer> solves = new HashMap<>();
+
+        Set<Node> nodesToEvaluate = new HashSet<>();
+        Set<Node> nextNodes = new HashSet<>();
+
+        nextNodes.add(new Node( new Coordinate(0, 1), Direction.RIGHT, 0, ""));
+        nextNodes.add(new Node( new Coordinate(1, 0), Direction.DOWN, 0, ""));
+
+        AtomicReference<Integer> bestResult = new AtomicReference<>(Integer.MAX_VALUE);
+        AtomicReference<String> bestResultPath = new AtomicReference<>("");
+
+        while (!nextNodes.isEmpty()) {
+
+            nodesToEvaluate.addAll(nextNodes);
+            nextNodes.clear();
+
+            nodesToEvaluate.forEach(node -> {
+
+                // if our node is out of bounds we can stop already
+                try {
+                    Integer test = grid[node.startCoordinate.line()][node.startCoordinate.column()];
+                } catch (Exception e) {
+                    return;
+                }
+
+                // check if we've been here before
+                if (solves.get(node.getHashKey()) == null) {
+                    solves.put(node.getHashKey(), node.heatLost());
+                } else {
+                    if (solves.get(node.getHashKey()) <= node.heatLost()) {
+                        // we've been here already and found a faster or equal solution
+                        return;
+                    } else {
+                        solves.put(node.getHashKey(), node.heatLost());
+                    }
+                }
+
+                Integer newHeatLost = node.heatLost();
+
+                // try to populate the next potential nodes
+                try {
+                    Coordinate noStep = node.startCoordinate().step(node.startDirection(), 0);
+                    newHeatLost += grid[noStep.line()][noStep.column()];
+
+                } catch (Exception ignored) { return; }
+
+                try {
+                    Coordinate oneStep = node.startCoordinate().step(node.startDirection(), 1);
+                    newHeatLost += grid[oneStep.line()][oneStep.column()];
+
+                } catch (Exception ignored) { return; }
+
+                try {
+                    Coordinate twoStep = node.startCoordinate().step(node.startDirection(), 2);
+                    newHeatLost += grid[twoStep.line()][twoStep.column()];
+
+                } catch (Exception ignored) { return; }
+
+                for (int i = 3; i < 10; i++) {
+                    try {
+                        Coordinate threeStep = node.startCoordinate().step(node.startDirection(), i);
+                        newHeatLost += grid[threeStep.line()][threeStep.column()];
+
+                        if (threeStep.line().equals(finalCoordinate.line()) && threeStep.column().equals(finalCoordinate.column())) {
+                            if (newHeatLost < bestResult.get()) {
+                                bestResult.set(newHeatLost);
+                                bestResultPath.set(node.history());
+                            }
+                            return;
+                        }
+
+                        nextNodes.add(new Node(threeStep.step(node.startDirection().clockwise(), 1), node.startDirection().clockwise(), newHeatLost, node.history + ">" + node.getHashKey()));
+                        nextNodes.add(new Node(threeStep.step(node.startDirection().counterClockwise(), 1), node.startDirection().counterClockwise(), newHeatLost, node.history + ">" + node.getHashKey()));
+                    } catch (Exception ignored) { return; }
+                }
+
+            });
+
+            nodesToEvaluate.clear();
+
+        }
+
+        return bestResult.toString();
     }
 
     public enum Direction {
